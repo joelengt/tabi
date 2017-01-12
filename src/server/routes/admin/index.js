dc536c7e18f6b3fa361bf6ad1bc4bd2bad0b6cb1
joelengt <joelengt@gmail.com>
2017-01-12 18:18:30 -0500

    Add semicolon

--- src/server/routes/admin/index.js
+++ src/server/routes/admin/index.js
@@ -4,7 +4,7 @@ var route = express.Router();
 var namingListController = require('../../controllers/NamingListController/index.js');
 var NamingListController = new namingListController();
 
-route.get('/list', NamingListController.list)
+route.get('/list', NamingListController.list);
 route.get('/get-info', NamingListController.getInfo);
 
 module.exports = route;
