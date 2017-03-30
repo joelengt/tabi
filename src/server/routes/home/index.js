849305999d6c0728cd8182b03bda3621d35ea643
joelengt <joelengt@gmail.com>
2017-03-30 13:21:06 -0500

    Update template home values

--- src/server/routes/home/index.js
+++ src/server/routes/home/index.js
@@ -11,70 +11,69 @@ route.get('/', function (req, res) {
 
     var id_user = '312312312';
 
-    generatePDF(id_user, (err, result) => {
-        if(err) {
-            return console.log('error', err);
-        }
-        console.log('PDF final terminado');
-        console.log(result);
+    // generatePDF(id_user, (err, result) => {
+    //     if(err) {
+    //         return console.log('error', err);
+    //     }
+    //     console.log('PDF final terminado');
+    //     console.log(result);
 
 
-    });
+    // });
 
-    // res.render('./home/index.jade');
-
-
-    htmlTo(`
-      <style type="text/css">
-        table td {
-          color: #666;
-          height: 20px;
-          background-color: #f1f1f1;
-          border: 1px solid #eee;
-        }
-      </style>
-      <table>
-        <tr>
-          <td>foo</td>
-          <td>bar</td>
-        </tr>
-        <tr>
-          <td>hello</td>
-          <td>world</td>
-        </tr>
-        <tr>
-          <td type="number">123</td>
-          <td type="number">123.456</td>
-        </tr>
-        <tr>
-          <td data-type="bool">true</td>
-          <td data-type="bool">false</td>
-        </tr>
-        <tr>
-          <td data-type="bool">1</td>
-          <td data-type="bool">0</td>
-        </tr>
-        <tr>
-          <td type="formula">SUM(A1:B1)</td>
-          <td type="formula">A1-B1</td>
-        </tr>
-        <tr>
-          <td type="date">2013-01-12T12:34:56+08:00</td>
-          <td type="datetime">2013-01-12T12:34:56+08:00</td>
-        </tr>
-      </table>
-    `, (err, file) => {
-      if (err) return console.error(err);
+    // htmlTo(`
+    //   <style type="text/css">
+    //     table td {
+    //       color: #666;
+    //       height: 20px;
+    //       background-color: #f1f1f1;
+    //       border: 1px solid #eee;
+    //     }
+    //   </style>
+    //   <table>
+    //     <tr>
+    //       <td>foo</td>
+    //       <td>bar</td>
+    //     </tr>
+    //     <tr>
+    //       <td>hello</td>
+    //       <td>world</td>
+    //     </tr>
+    //     <tr>
+    //       <td type="number">123</td>
+    //       <td type="number">123.456</td>
+    //     </tr>
+    //     <tr>
+    //       <td data-type="bool">true</td>
+    //       <td data-type="bool">false</td>
+    //     </tr>
+    //     <tr>
+    //       <td data-type="bool">1</td>
+    //       <td data-type="bool">0</td>
+    //     </tr>
+    //     <tr>
+    //       <td type="formula">SUM(A1:B1)</td>
+    //       <td type="formula">A1-B1</td>
+    //     </tr>
+    //     <tr>
+    //       <td type="date">2013-01-12T12:34:56+08:00</td>
+    //       <td type="datetime">2013-01-12T12:34:56+08:00</td>
+    //     </tr>
+    //   </table>
+    // `, (err, file) => {
+    //   if (err) return console.error(err);
       
-      file.saveAs()
-        .pipe(fs.createWriteStream('test.xlsx'))
-        .on('finish', () => console.log('Done.'));
-    });
-    
-    res.status(200).json({
-        status: 'ok',
-        message: 'value'
-    })
+    //   file.saveAs()
+    //     .pipe(fs.createWriteStream('test.xlsx'))
+    //     .on('finish', () => console.log('Done.'));
+    // });
+
+    // res.status(200).json({
+    //     status: 'ok',
+    //     message: 'value'
+    // })
+
+    res.render('./home/index.jade');
 
 });
 
