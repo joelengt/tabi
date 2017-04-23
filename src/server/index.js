94258cd5315bc4b67cd34657203dadeae6e463c8
joelengt <joelengt@gmail.com>
2017-04-23 11:13:55 -0500

    Add process.env.port before config.port

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,7 @@ function middleware (req, res, next) {
 }
 
 // Configuración del servidor
-app.set('port', config.server.port || process.env.PORT);
+app.set('port', process.env.PORT || config.server.port);
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, './views'));
 
